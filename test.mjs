17f369a12c85d0fe819edbb5756c86ad3cd0d353
xn <280145668@qq.com>
2022-07-20 17:55:28 +0800

    push

--- test.mjs
+++ test.mjs
@@ -14,7 +14,7 @@ const sql = postgres({
   connect_timeout: 2,
 })
 const query = async (statement) => {
-  p(statement)
+  // p(statement)
   return await sql.unsafe(statement)
 }
 await query(`
@@ -27,47 +27,124 @@ CREATE TABLE usr (
   age integer);
 CREATE TABLE profile (
   id serial PRIMARY KEY,
-  usr_name varchar(4) REFERENCES "usr" ("name") ON DELETE NO ACTION ON UPDATE CASCADE,
+  usrName varchar(4) REFERENCES "usr" ("name") ON DELETE NO ACTION ON UPDATE CASCADE,
   info varchar(50));
 `)
-let usr = Model.makeClass({
+const Usr = Model.makeClass({
   query,
   tableName: "usr",
   fields: {
-    name: { label: "姓名",unique: true, maxlength:4, minlength:1 },
-    age: { label: "年龄", type:"integer", max:100, min:1 },
+    name: { label: "姓名", unique: true, maxlength: 4, minlength: 1 },
+    age: { label: "年龄", type: "integer", max: 100, min: 1 },
   },
 });
-let profile = Model.makeClass({
+const Profile = Model.makeClass({
   query,
   tableName: "profile",
   fields: {
-    usr_name: { label: "用户", reference: usr, referenceColumn: 'name' },
-    info: { label: "信息", maxlength:50 },
+    usrName: { label: "用户", reference: Usr, referenceColumn: 'name' },
+    info: { label: "信息", maxlength: 50 },
   },
 });
-const usr_rows = [{ "name": "tom", "age": 22 }, { "name": "kate", "age": 21 }, { "name": "mike", "age": 11 }]
-await usr.merge(usr_rows, "name")
-let res = await usr.all()
-let select_u = await usr.select("name", "age").where({age__lt:20}).execr()
-test('test select where', () => {
-  for (const u of select_u) {
+
+const firstUser = { "name": "tom", "age": 22 }
+await Usr.insert(firstUser).execr()
+const usrs_after_insert = await Usr.all()
+test('test insert one row', () => {
+  expect(usrs_after_insert).toEqual([{ id: 1, ...firstUser }])
+});
+
+const bulkInsertUsers = [{ "name": "kate", "age": 21 }, { "name": "mike", "age": 11 }]
+const allUsers = [firstUser, ...bulkInsertUsers]
+await Usr.insert(bulkInsertUsers).execr()
+const usrs_after_bulk_insert = await Usr.all()
+test('test bulk insert', () => {
+  for (const u of usrs_after_bulk_insert) {
+    expect(u).toMatchObject(allUsers.find(e=>e.name==u.name))
+  }
+});
+
+const mergedUsers = [...allUsers, { "name": "foo", "age": 11 }]
+await Usr.merge(mergedUsers, "name")
+const usrs_after_merge = await Usr.all()
+test('test merge - update && insert', () => {
+  expect(usrs_after_merge.length).toBe(4)
+  for (const u of usrs_after_merge) {
+    expect(u).toMatchObject(mergedUsers.find(e => e.name == u.name))
+  }
+});
+
+await Usr.delete({ name: "foo" }).execr()
+const users_after_delete = await Usr.all()
+test('test delete', () => {
+  expect(users_after_delete.length).toBe(3)
+  for (const u of users_after_delete) {
+    expect(u).toMatchObject(allUsers.find(e => e.name == u.name))
+  }
+});
+
+const mergedUpdateUsers = [{ "name": "kate", "age": 50 }, { "name": "tom", "age": 60 }]
+await Usr.merge(mergedUpdateUsers, "name")
+const users_after_merge_update = await Usr.all()
+test('test no new rows', () => {
+  expect(users_after_merge_update.length).toBe(3)
+})
+const kate = await Usr.select("age").where({ name: "kate" }).get()
+test('test kate age changed and select age only', () => {
+  expect(kate).toEqual({ age: 50 })
+})
+const tom = await Usr.select("age").get({ name: "tom" })
+test('test tom age changed and select age only and condition in get', () => {
+  expect(tom).toEqual({ age: 60 })
+})
+
+await Usr.update({age: 66}).where({name: "kate"}).execr()
+const usrKate = await Usr.get({ name: "kate" })
+test('test update by where', () => {
+  expect(usrKate.age).toEqual(66)
+})
+
+const updatesUsers = [{ "name": "tom", "age": 22 }, { "name": "kate", "age": 21 }, { "name": "foo", "age": 21 }]
+await Usr.updates(updatesUsers, "name")
+const users_after_updates = await Usr.all()
+test('test no new rows after sql.updates', () => {
+  expect(users_after_updates.length).toBe(3)
+})
+const kate2 = await Usr.select("age").where({ name: "kate" }).get()
+test('test kate age changed back by updates', () => {
+  expect(kate2).toEqual({ age: 21 })
+})
+const tom2 = await Usr.select("age").where({ name: "tom" }).get()
+test('test tom age changed back by updates', () => {
+  expect(tom2).toEqual({ age: 22 })
+})
+const no_foo = await Usr.select("age").where({ name: "foo" }).execr()
+test('test foo is not inserted in sql.updates', () => {
+  expect(no_foo.length).toBe(0)
+})
+const ageLt20Users = await Usr.select("name", "age").where({ age__lt: 20 }).execr()
+test('test select where by age less than', () => {
+  for (const u of ageLt20Users) {
     expect(u).toMatchObject({ "name": "mike", "age": 11 })
   }
 });
-let update_u = await usr.update({ age: 30 }).where({ name: "kate" }).execr()
-let u30 = await usr.get({ name: "kate" })
+
+await Usr.update({ age: 30 }).where({ name: "kate" }).execr()
+const u30 = await Usr.get({ name: "kate" })
 test('test update where and get', () => {
   expect(u30).toMatchObject({ "name": "kate", "age": 30 })
 });
-test('test merge', () => {
-  for (const u of res) {
-    expect(u).toMatchObject(usr_rows.find(e=>e.name==u.name))
-  }
+
+const profile_rows = [{ usrName: "tom", info: "dad" }, { usrName: "kate", info: "mom" }]
+await Profile.merge(profile_rows, "usrName")
+const p1 = await Profile.where({ usrName__age: 30 }).get()
+test('test join get', () => {
+  expect(p1).toMatchObject({ "name": "kate", "age": 30 })
 });
-const profile_rows = [{ usr_name: "tom", info: "dad" }, { usr_name: "kate", info: "mom" }]
-await profile.merge(profile_rows, "usr_name")
-p(await profile.all())
-let p1 = await profile.where({ usr_name__age__lt: 22 }).execr()
-p({p1})
+const usave = await Usr.saveCreate({ name: "u1", age: 50 })
+p({ usave })
+const u1 = await Usr.get({ name: "u1" })
+test('test saveCreate', async () => {
+  expect(u1).toMatchObject({ name: "u1", age: 50 })
+});
 await sql.end()

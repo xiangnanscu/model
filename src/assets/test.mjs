77bd2d54a968b7af86cad847d9c948395dc51318
xiangnan <280145668@qq.com>
2024-07-18 17:32:46 +0800

    fix:self-join-bugs

--- src/assets/test.mjs
+++ src/assets/test.mjs
@@ -1,7 +1,12 @@
 Usr.where({ name: "kate" });
 Usr.where({ parent__name: "kate" });
-Profile.update({ info: "big kids" }).where({ usr__age__gt: 12 }).returning("info");
+Usr.where({ parent__age: 20 });
+Usr.where({ parent__parent__age: 40 });
 Profile.where({ usr__age__gt: 12 }).select("info");
+Profile.where({ usr__age__gt: 12, usr__parent__age__gt: 32 }).select("info");
+Profile.where({ usr__parent__age__gt: 12 }).select("info");
+Profile.update({ info: "big kids" }).where({ usr__age__gt: 12 }).returning("info");
+
 Usr.insert({ name: "tom", age: 1 });
 Usr.insert([
   { name: "tom", age: 1 },
